e51124c234e832e9595ac74970a22af6bcade4ce
Gabriel Hrastovic <gabrielcro2369@gmail.com>
2022-07-13 07:19:08 -0400

    Homepage styling, removed background image for more simplicity and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
-import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#76b900',
+      
+    },
+    secondary: {
+      main: '#ffffff',
+    },
+  },
+});
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-        <Typography variant="h4" color="#76b900">NVIDIA GRAPHICS CARDS</Typography>
-        <Typography variant="subtitle1" color="white">NVIDIA RTX™ graphics cards are bringing the power of real-time ray tracing and AI to the applications you use every day.</Typography>
+        <Box sx={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        <Typography variant="h4" color="primary">NVIDIA GRAPHICS CARDS</Typography>
+        <Typography variant="subtitle1" color="secondary">NVIDIA RTX™ graphics cards are bringing the power of real-time ray tracing and AI to the applications you use every day.</Typography>
+        <Button sx={{color: 'white', fontWeight: 'bold', borderRadius: '0px'}}variant="contained" color="primary" >View Store</Button>
+        </Box>
     </div>
+    </ThemeProvider>
   );
 }
 
