f5d01c333d28ffc75229780d9f9af6fd33bc4f62
Gabriel Hrastovic <gabrielcro2369@gmail.com>
2022-07-13 06:31:27 -0400

    Initial set up, products and background images downloaded and placed in images foloder, MUI installed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import '@fontsource/roboto/300.css';
+import Typography from '@mui/material/Typography';
+
+function App() {
+  return (
+    <div className="App">
+        <Typography variant="h4" color="#76b900">NVIDIA GRAPHICS CARDS</Typography>
+        <Typography variant="subtitle1" color="white">NVIDIA RTX™ graphics cards are bringing the power of real-time ray tracing and AI to the applications you use every day.</Typography>
+    </div>
+  );
+}
+
+export default App;
